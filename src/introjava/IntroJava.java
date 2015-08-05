247bcb2687be7c7045dabe80ff9363d1e4283862
noite <noite@DPC2013-3.aluno.local>
2015-08-05 20:14:29 -0300

    Inicializando GitHub Projeto.

--- src/introjava/IntroJava.java
+++ src/introjava/IntroJava.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package introjava;
 
-/**
- *
- * @author noite
- */
+import java.util.Scanner;
+
+
 public class IntroJava {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+     
+        Scanner teste = new Scanner(System.in);
+        System.out.println("Informe um valor");
+        int retorno = teste.nextInt();
+        
+        while (retorno != 0){
+            System.out.println("Informe outro valor");
+            retorno = teste.nextInt();
+        }
+        System.out.println("Informado com sucesso!");
+        
     }
     
 }
